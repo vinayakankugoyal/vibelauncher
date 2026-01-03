d07ff6cfee5c83ecf035e9d310ccfedf14361388
Vinayak Goyal <vinayakankugoyal@gmail.com>
2026-01-03 02:16:45 -0800

    bump version code to 11 and version name to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pizza.vibelauncher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
