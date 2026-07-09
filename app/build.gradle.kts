d48c0cf5e765747ac499aea8feacfb3b7010a7e7
Vinayak Goyal <vinayakankugoyal@gmail.com>
2026-07-09 15:02:17 -0400

    bump version to 1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pizza.vibelauncher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.10"
+        versionCode = 13
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
